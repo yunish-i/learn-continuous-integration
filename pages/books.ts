29d0b3fa45db4920aac0ab98db8916c6d546ef42
yunish-i <yunyiii1376@outlook.com>
2025-04-08 14:57:40 -0400

    Update books.ts

--- pages/books.ts
+++ pages/books.ts
@@ -1,32 +1,33 @@
-import Book from '../models/book';
-import Author from '../models/author';
-import express from 'express';
+import Book from "../models/book";
+import Author from "../models/author";
+import express from "express";
 
 const router = express.Router();
 
-
 const showBooks = async (): Promise<string[] | void> => {
   try {
-    const books = await Book.getAllBooksWithAuthors('title author', {title: 1});
+    const books = await Book.getAllBooksWithAuthors("title author", {
+      title: 1,
+    });
     return books.map((b) => {
       const authorName = new Author(b.author).name; // Assuming 'Author' returns the author's name
       return `${b._id} : ${b.title} : ${authorName}`;
     });
   } catch (err) {
-    console.log('Could not get books ' + err);
+    console.log("Could not get books " + err);
   }
-}
+};
 /**
  * @route GET /books
  * @returns an array of strings, where each string contains the book ID, title, and author name
  * @returns - a message indicating that no books were found if an error occurs
  */
-router.get('/', async (_, res) => {
+router.get("/", async (_, res) => {
   try {
     const data = await showBooks();
     res.send(data);
   } catch {
-    res.send('No books found');
+    res.send("No books found");
   }
 });
 
